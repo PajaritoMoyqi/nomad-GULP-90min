dac9d02475e03827c3ca4bd4adb50cb817ac5651
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-03-04 19:10:33 +0900

    webserver added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,14 @@
 import gulp from 'gulp';
 import gpug from 'gulp-pug';
 import del from 'del';
+import ws from 'gulp-webserver';
 
 // index.pug to html
 
 const routes = {
   pug: {
-    src: './src/*.pug',
-    dest: './build',
+    src: 'src/*.pug',
+    dest: 'build/',
   },
 };
 
@@ -17,15 +18,20 @@ const pug = () => {
     .pipe(gulp.dest(routes.pug.dest));
 }
 
+const webserver = () => gulp.src(routes.pug.dest).pipe(ws({livereload: true, open: true}));
+
 // clean the build folder
 
 const clean = () => {
   return del([routes.pug.dest]);
 }
 
+// series
+
 const prepare = gulp.series([clean]);
 const assets = gulp.series([pug]);
+const postDev = gulp.series([webserver]);
 
 // main function
 
-export const dev = gulp.series([prepare, assets]);
+export const dev = gulp.series([prepare, assets, postDev]);
